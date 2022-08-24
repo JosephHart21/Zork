0f835d1e032e757c68db3e58e9ed4e9a64f78582
Joseph Hart <107864631+JosephHart21@users.noreply.github.com>
2022-08-24 15:12:20 -0400

    Adds first input scenario
    Used a switch case opposed to if-else's.

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -9,6 +9,18 @@ namespace Zork
 
             Console.WriteLine("Welcome to Zork!");
 
+            string inputString = Console.ReadLine().Trim().ToUpper();
+
+            switch (inputString){
+                case "QUIT": Console.WriteLine("Thank you for playing."); 
+                    break;
+                case "LOOK": Console.WriteLine("This is an open field west of a white house, with a boarded front door.\nA rubber mat saying 'Welcom to Zork!' lies by the door."); 
+                    break;
+                case "": Console.WriteLine("Must type command"); 
+                    break;
+                default: Console.WriteLine($"'{inputString}' is not a recognized command."); 
+                    break;
+            }
         }
     }
 }
