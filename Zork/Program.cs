f7acb779c1d31f0a667a907099e73f240b3f30ee
Joseph Hart <107864631+JosephHart21@users.noreply.github.com>
2022-08-29 15:19:38 -0400

    Zork!1.1
    Monday Class modifications: parsing commandString into the new Enum.

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Zork
 {
@@ -6,21 +6,43 @@ namespace Zork
     {
         static void Main()
         {
-
             Console.WriteLine("Welcome to Zork!");
+			
+			string inputString = Console.ReadLine().Trim();
+			
+			Commands command; command = ToCommand(inputString);
 
-            string inputString = Console.ReadLine().Trim().ToUpper();
-
-            switch (inputString){
-                case "QUIT": Console.WriteLine("Thank you for playing."); 
+            switch (commad)
+			{
+                case Commands.Quit: Console.WriteLine("Thank you for playing."); 
                     break;
-                case "LOOK": Console.WriteLine("This is an open field west of a white house, with a boarded front door.\nA rubber mat saying 'Welcom to Zork!' lies by the door."); 
+					
+                case Commands.Look: Console.WriteLine("This is an open field west of a white house, with a boarded front door.\nA rubber mat saying 'Welcom to Zork!' lies by the door."); 
                     break;
-                case "": Console.WriteLine("Must type command"); 
+					
+                case Commands.Unknown: Console.WriteLine("Must type command"); 
                     break;
+					
                 default: Console.WriteLine($"'{inputString}' is not a recognized command."); 
                     break;
             }
+			
         }
+		
+		static Commands ToCommand(string commmandString)
+		{
+		
+			if(Enum.TryParse<Commands>(commmandString, true, out Commands command))
+			{
+				return command;
+			}
+			else
+			{
+				return Commands.Unknown;
+			}
+			
+		}
+		
     }
+	
 }
