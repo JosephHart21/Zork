6455476ea8286dcc769c5e65b794177f8098b46a
Joseph Hart <107864631+JosephHart21@users.noreply.github.com>
2022-09-19 13:57:31 -0400

    Descriptions
    Adds unfinished descriptions

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -86,26 +86,26 @@ namespace Zork
 			return didMove;
 		}
 
-		private static void InitializeRoomDescriptions()
-		{
-			_rooms[0,0].Description = "";
-            _rooms[0,1].Description = "";
-            _rooms[0,2].Description = "";
-
-            _rooms[1,0].Description = "";
-            _rooms[1,1].Description = "";
-            _rooms[1,2].Description = "";
-
-            _rooms[2,0].Description = "";
-            _rooms[2,1].Description = "";
-            _rooms[2,2].Description = "";
-        }
-
         private static readonly Room[,] _rooms = {
             {new Room("Rocky Trail"), new Room ("South of House"), new Room ("Canyon View")},
             {new Room ("Forest"),new Room ("West of House"),new Room ("Behind House")},
             {new Room ("Dense Woods"),new Room ("North of House"),new Room ("Clearing")}
         };
+
+		private static void InitializeRoomDescriptions()
+		{
+			_rooms[0,0].Description = "You are on a rock-stream trail.";
+            _rooms[0,1].Description = "You are facing the south side of a white house.";
+            _rooms[0,2].Description = "You are at the top of the Great Canyon on its south wall.";
+
+            _rooms[1,0].Description = "This is a forest, with trees in all directions around you.";
+            _rooms[1,1].Description = "This is an open field west of a white house;";
+            _rooms[1,2].Description = "You are behind the white house.";
+
+            _rooms[2,0].Description = "This is a dimly lit forest, with large trees all around.";
+            _rooms[2,1].Description = "You are facing the north side of a white house.";
+            _rooms[2,2].Description = "You are in a clearing, with a forest surrounding you on the west;";
+        }
 
         private static Location _location = new Location() { Row = 1, Column = 1 };
 
@@ -113,7 +113,7 @@ namespace Zork
         {
             get
             {
-                return _rooms[_location.Row, _location.Column];
+				return _rooms[_location.Row, _location.Column];
             }
         }
     }
