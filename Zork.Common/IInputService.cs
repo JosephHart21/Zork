b37c59220b663fc256cf7be617a4268a4eea9ab3
Joseph Hart <107864631+JosephHart21@users.noreply.github.com>
2022-11-07 14:23:42 -0500

    Update
    Added inputservice

---
+++ Zork.Common/IInputService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zork.Common
+{
+    internal class IInputService
+    {
+
+        event EventHandler<string> InputReceived;
+    }
+}
