5896a051d0d8a30fc37d36e80dda081dabb5d1e0
Joseph Hart <107864631+JosephHart21@users.noreply.github.com>
2022-11-14 14:34:35 -0500

    Unity Zork JSON
    Deserializes the game into Unity Zork

--- Zork.Unity/Assets/Scripts/GameManager.cs
+++ Zork.Unity/Assets/Scripts/GameManager.cs
@@ -1,10 +1,15 @@
 using UnityEngine;
 using Zork.Common;
+using Newtonsoft.Json;
 
 public class GameManager : MonoBehaviour
 {
     private void Awake()
     {
-        
+        TextAsset gameJson = Resources.Load<TextAsset>("GameJson");
+        _game = JsonConvert.DeserializeObject<Game>(gameJson.text);
     }
+
+    private Game _game;
+
 }
